e3aac19d24d066c7dcdfe7b8384a7da00ec65737
KID <50666207+KID421@users.noreply.github.com>
2019-11-16 17:00:49 +0800

    認識 API

---
+++ UnityProject/Assets/Scripts/LearnAPI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    // 欄位
+    public Transform transformA;
+    public Transform transformB;
+
+    public Camera cam;
+
+    private void Start()
+    {
+        // 使用靜態成員 - 屬性
+        print("隨機：" + Random.value);
+
+        // 使用靜態成員 - 屬性 數學類別.PI (Mathf)
+        print("PI：" + Mathf.PI);
+
+        // 使用靜態成員 - 方法
+        print("隨機範圍：" + Random.Range(1, 100));
+
+        print("絕對值：" + Mathf.Abs(-99));
+
+        // 非靜態成員 - 屬性
+        // 需要先給予欄位存放實體物件
+        print("物件 A 的座標：" + transformA.position);
+
+        transformB.position = new Vector3(-2, 0, 0);
+        print("物件 B 的座標：" + transformB.position);
+
+        cam.depth = 10;
+        print("攝影機深度：" + cam.depth);
+    }
+
+    private void Update()
+    {
+        // 非靜態成員 - 方法
+        transformB.Rotate(0, 0, 10);
+    }
+}
---
+++ UnityProject/Assets/Scripts/LearnIF.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class LearnIF : MonoBehaviour
+{
+    public bool test;
+    public string prop;
+    public float hp = 100;
+
+    // 更新事件：一秒執行 60 次
+    private void Update()
+    {
+        #region 練習 if
+        // 判斷式 if 語法
+        // if (布林值) { 陳述式或演算法 }
+        // () 裡面的布林值為 true 才會執行 if {}
+        if (test)
+        {
+            print("打開開關~");
+        }
+        // () 裡面的布林值為 false 才會執行 else {}
+        else
+        {
+            print("關閉開關~");
+        }
+
+        if (prop == "紅色藥水")
+        {
+            print("補血");
+        }
+        else if (prop == "藍色藥水")
+        {
+            print("補魔力");
+        }
+        else if (prop == "黃色藥水")
+        {
+            print("補體力");
+        }
+        else
+        {
+            print("沒發生任何事~");
+        }
+        #endregion
+        if (hp >= 70)
+        {
+            print("安全");
+        }
+        else if (hp >= 50)
+        {
+            print("警告");
+        }
+        else if (hp >= 20)
+        {
+            print("危險");
+        }
+        else
+        {
+            print("快死了!!!");
+        }
+    }
+}
